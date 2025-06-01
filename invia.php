c526bb46bada3361c6ff7e2e394f8ece65187b82
OmarF027 /c/wamp64/www/miei-progetti/calcolatrice (main)$ git config --global user.name OmarF027 <omarfrosali027@gmail.com>
2025-06-01 22:20:13 +0200

    Aggiorno

--- invia.php
+++ invia.php
@@ -80,7 +80,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['lavora_form'])) {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>Invio Candidatura | 21OVEN</title>
+    <title>Invio Candidatura | GOL D. RAMEN</title>
 
     <!-- Font e Icone -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" />
